3885d5e36dcdfcb6e82f34812f13e56dd1bb7b86
wuxianqiang <2631640352@qq.com>
2018-03-05 12:33:42 +0800

    项目第六天

---
+++ src/api/config.js
@@ -0,0 +1,33 @@
+export const commonParams = {
+  g_tk: 1928093487,
+  inCharset: 'utf-8',
+  outCharset: 'utf-8',
+  notice: 0,
+  format: 'jsonp'
+}
+
+export const opts = {
+  param: 'jsonpCallback'
+}
+
+export const opts1 = {
+  param: 'jsonpCallback',
+  name: 'getPlaylist'
+}
+
+export const opts2 = {
+  param: 'jsonpCallback',
+  name: 'GetSingerListCallback'
+}
+
+export const opts3 = {
+  param: 'jsonpCallback',
+  name: 'MusicJsonCallbacksinger_track'
+}
+
+export const opts4 = {
+  param: 'jsonpCallback',
+  name: 'playlistinfoCallback'
+}
+
+export const ERR_OK = 0
---
+++ src/store/state.js
@@ -0,0 +1,16 @@
+import {playMode} from 'common/js/config'
+
+const state = {
+  singer: {},
+  toindex: 1,
+  playing: false,
+  fullScreen: false,
+  playList: [],
+  sequenceList: [],
+  mode: playMode.sequence,
+  currentIndex: -1,
+  disc: {},
+  topList: {}
+}
+
+export default state
